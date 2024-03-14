7b28539a84c87e94db7693a1753d1bfa23e47750
Mihai <mihai807@mail.ru>
2024-03-14 19:15:26 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrap">
+    <nav>
+        <RouterLink to="/">Все задачи</RouterLink>
+        <RouterLink to="/new">Запланированные</RouterLink>
+        <RouterLink to="/complete">Завершенные</RouterLink>
+      </nav>
+      <RouterView />
+  </div>
+ 
+</template>
+
+<style scoped>
+.wrap {
+  display: flex;
+  /* height: 100vh; */
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: HomeView
+    },
+    {
+      path: '/new',
+      component: AboutView
+    },
+    {
+      path: '/complete',
+      component: AboutView
+    }
+  ]
+})
+
+export default router
